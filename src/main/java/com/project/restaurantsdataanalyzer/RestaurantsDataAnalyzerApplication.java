3ad6768c9a18b666bc12f68af095de80a1211398
RustamBaimukanov <watashirustam@gmail.com>
2021-10-18 22:28:27 +0300

    Initial commit

---
+++ src/main/java/com/project/restaurantsdataanalyzer/RestaurantsDataAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.project.restaurantsdataanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantsDataAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantsDataAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/restaurantsdataanalyzer/RestaurantsDataAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.restaurantsdataanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantsDataAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
